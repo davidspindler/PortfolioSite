34cd88a2c5a5e963c64981d8f5b15758cd1575f8
David <davidspindler612@gmail.com>
2022-10-16 14:40:26 -0700

    probably adding google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,24 @@
-import Theme from '../styles/theme';
-
+import Theme from "../styles/theme";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
         <Component {...pageProps} />
       </Theme>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SL1YQ9EW16"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || []
+        function gtag(){dataLayer.push(arguments);
+          gtag('js', new Date())
+          
+          gtag('config', 'G-SL1YQ9EW16');
+          `}
+      </Script>
     </>
   );
 }
- 
